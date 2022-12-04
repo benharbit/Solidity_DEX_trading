c3deecd31938e2d683de79f535f53d25150c47a5
Omar Benharbit <oab102@hotmail.com>
2022-12-04 16:11:19 -0500

    update test

---
+++ tests/trading.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import hre from "hardhat";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
+
+describe("Arbitrade", function () {
+  it("Should complete arb with minimum profit", async function () {
+    // TODO: Write test
+  });
+});
